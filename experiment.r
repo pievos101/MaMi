3479b7c5daa270583b193ab087712025d1ceb738
pievos101 <bastianxpfeifer@gmail.com>
2024-03-14 10:31:52 +0100

    up date

--- experiment.r
+++ experiment.r
@@ -1,10 +1,20 @@
 #
 source("mami.r")
-data(iris)
-DATA = iris[,-5]
-labels = as.numeric(iris[,5])
+source("mami_crossval.r")
+source("get_dataset.r")
+library(mlbench)
 
-n_iter = 50 
+res = get_dataset("YEAST")
+DATA  = res$train
+labels = res$target
+t = table(labels)
+ids = which(t<=5)
+LL = as.numeric(names(t[ids]))
+ids = !is.element(labels, LL)
+DATA = DATA[ids,]
+labels = labels[ids]
+
+n_iter = 100 
 RES = matrix(NaN, n_iter, 2)
 
 for(xx in 1:n_iter){
@@ -15,25 +25,39 @@ for(xx in 1:n_iter){
     test  = DATA[-ids,]
     train_labels = labels[ids]
     test_labels = labels[-ids]
+    if(length(setdiff(unique(train_labels), unique(test_labels)))!=0){
+        #print("Problem")
+        #xx = xx - 1
+        next
+    }
     rownames(train) = 1:nrow(train)
     rownames(test) = (nrow(train)+1):(nrow(train)+nrow(test))
 
-    res = mami(train, test, train_labels)
-    pred = res$prediction
-
+    #res = mami_crossval(train, test, train_labels)
+    #print(res$k1);print(res$k2);
+    res = mami(train, test, train_labels, k1=5, k2=10)
+    pred   = res$prediction
+    pred2  = res$coverage
+    colnames(pred2) = sort(unique(train_labels))
+    #ids = which(pred==0)
+    #pred2[ids] = 1-pred2[ids]
     library(aricode)
-    MAMI_perf = ARI(pred, test_labels)
+    library(pROC)
+    MAMI_perf = multiclass.roc(test_labels, pred2)$auc[1]
+    #ARI(pred, test_labels)
 
     # Now with caret
     library(caret)
     #ctrl <- trainControl(method="repeatedcv",repeats = 3) #,classProbs=TRUE,summaryFunction = twoClassSummary)
     #knnFit <- train(Direction ~ ., data = training, method = "knn", trControl = ctrl, preProcess = c("center","scale"), tuneLength = 20)
-    knnFit <- train(x=train, y=train_labels, 
+    knnFit <- train(x=train, y=as.factor(train_labels), 
                     method = "knn",
                     preProcess =  c("center","scale"),
                     tuneGrid=data.frame(k=5))
-    knnPredict <- predict(knnFit, newdata = test)
-    KNN_perf = ARI(knnPredict, test_labels)
+    knnPredict <- predict(knnFit, newdata = test, type = "prob")
+    #knnPredict <- knnPredict[,2]
+    colnames(knnPredict) = sort(unique(train_labels))
+    KNN_perf = multiclass.roc(test_labels, knnPredict)$auc[1] #ARI(knnPredict, test_labels)
 
 
 RES[xx,1] = MAMI_perf
--- mami.r
+++ mami.r
@@ -1,8 +1,8 @@
-mami <- function(train, test, train_labels, k1=5, k2=5){
+mami <- function(train, test, train_labels, k1=5, k2=10){
 
 D  = rbind(train, test)
 D1 = as.matrix(dist(scale(D)))
-
+#D1 = as.matrix(dist(D))
 
 # Calculate first layer neigborhood for each test point
 K1 = matrix(NaN,nrow(test),k1)
@@ -18,6 +18,8 @@ for (xx in (nrow(train)+1):(nrow(train)+nrow(test))){
     #print(vec_s)
 
 }
+#print(K1_l)
+#print(K1)
 
 # Calculate Second layer Neighborhood for each point 
 K2 = matrix(NaN, k1, k2)
@@ -32,6 +34,7 @@ for(xx in 1:nrow(K1)){
   K2_list[[xx]] = K2
   K2 = matrix(NaN, k1, k2)
 }
+#print(K2_list)
 
 C_list = list()
 C = rep(NaN, k1)
@@ -43,25 +46,41 @@ for(xx in 1:length(K2_list)){
     C_list[[xx]] = C
     C = rep(NaN, k1)
 }
+#print(C_list)
 
 COV_ALL = list()
 
 # Now infer the prediction
 for(xx in 1:nrow(K1_l)){
     l1 = table(K1_l[xx,])
+    maj = as.numeric(names(l1)[1])
     COV = rep(NaN, length(l1))
     names(COV) = names(l1)
     for(yy in 1:length(l1)){
         ids = which(K1_l[xx,] == as.numeric(names(l1[yy])))
+        #ids = which(K1_l[xx,] == maj)
+        #COV[yy] = sum(C_list[[xx]][ids])/(k1*k2)
         COV[yy] = sum(C_list[[xx]][ids])/(k1*k2)
     }
     #print(COV)
   COV_ALL[[xx]] = COV 
 }
 
+#print(COV_ALL)
 
 PRED = as.numeric(sapply(COV_ALL, function(x){names(which.max(x))}))
-COV  = as.numeric(sapply(COV_ALL, function(x){max(x)}))
+
+COV = matrix(0, length(PRED), length(unique(train_labels)))
+for(xx in 1:dim(COV)[1]){
+
+    ids = as.numeric(names(COV_ALL[[xx]])) #+ 1
+    #print(ids)
+    #print(COV_ALL[[xx]])
+    COV[xx, ids] = COV_ALL[[xx]]
+
+}
+
+#COV  = as.numeric(sapply(COV_ALL, function(x){max(x)}))
 
 return(list(prediction=PRED, coverage=COV))
 
